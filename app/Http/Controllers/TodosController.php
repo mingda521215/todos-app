415c9544686e74cac5ae2256ef70c1d8118fb1d4
mingda521215 <mingda@ms10.hinet.net>
2019-05-12 16:07:21 +0800

    Delete todo function

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -14,14 +14,17 @@ class TodosController extends Controller
         // 上述可簡寫為如下
         return view('todos.index')->with('todos', Todo::all());
     }
-    public function show($todoId)
+
+    public function show(Todo $todo)
     {
-        return view('todos.show')->with('todo', Todo::find($todoId));
+        return view('todos.show')->with('todo', $todo);
     }
+
     public function create()
     {
         return view('todos.create');
     }
+
     public function store()
     {
         $this->validate(request(), [
@@ -36,6 +39,35 @@ class TodosController extends Controller
         $todo->completed = false;
 
         $todo->save();
+
+        return redirect('/todos');
+    }
+
+    public function edit(Todo $todo)
+    {
+        return view('todos.edit')->with('todo', $todo);
+    }
+
+    public function update(Todo $todo)
+    {
+        $this->validate(request(), [
+            'name'        => 'required|min:6|max:12',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+
+        $todo->save();
+
+        return redirect('/todos');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
 
         return redirect('/todos');
     }
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -3,12 +3,12 @@
 Single Todo: {{ $todo->name }}
 @endsection
 @section('content')
-        <h1 class="text-center my-5">
-            {{ $todo->name }}
-        </h1>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-default">
+<h1 class="text-center my-5">
+{{ $todo->name }}
+</h1>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card card-default">
             <div class="card-header">
                 Details
             </div>
@@ -16,6 +16,8 @@ Single Todo: {{ $todo->name }}
                 {{ $todo->description }}
             </div>
         </div>
-            </div>
-        </div>
+        <a href="/todos/{{ $todo->id }}/edit" class="btn btn-info my-2">Edit</a>
+        <a href="/todos/{{ $todo->id }}/delete" class="btn btn-danger my-2">Delete</a>
+    </div>
+</div>
 @endsection
